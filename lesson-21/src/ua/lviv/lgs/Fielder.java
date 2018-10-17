2be83db51f3ce8d76416b108decbc48e0f9e20ac
LogosItAcademy <Tester99037@gmail.com>
2018-10-17 08:28:02 +0300

    added code example from lesson 22

--- lesson-21/src/ua/lviv/lgs/Fielder.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.lgs;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Fielder {
-	String params() default "private annotation";
-}
--- lesson-21/src/ua/lviv/lgs/StudentSocial.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.lgs;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface StudentSocial {
-	 String value() default "make it better";
-}
